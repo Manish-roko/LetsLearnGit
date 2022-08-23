bb007e592516bed87b73d7be9be3a60796a78464
Manish Sharma <manishmsharma21@gmail.com>
2022-08-23 23:05:10 +0530

    intial commit

---
+++ Add.java
@@ -0,0 +1,11 @@
+package com.p1.Sample;
+
+public class Add {
+	static int a = 90;
+	static int b = 10;
+
+	public static void main(String[] args) {
+
+		System.out.println("Sum of 2 number is :"+(a + b));
+	}
+}
